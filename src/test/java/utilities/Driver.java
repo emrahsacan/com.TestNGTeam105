bd707022a771b981f30b1abaa06a4f154dc298aa
emrahsacan <you@example.com>
2023-01-05 07:56:02 +0300

    5.01.2023

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,6 +1,5 @@
 package utilities;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,23 +7,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class Driver {
+
     static WebDriver driver;
+
     public static WebDriver getDriver(){
-        if (driver==null) {
+
+        if(driver==null) {
             WebDriverManager.chromedriver().setup();
-            driver=new ChromeDriver();
+            driver = new ChromeDriver();
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
+
         return driver;
+
     }
-    public static void closeDriver() {
-        if (driver!=null)
-        {
+
+    public static void closeDriver(){
+
+        if (driver != null){
             driver.close();
             driver=null;
         }
 
     }
 }
-
