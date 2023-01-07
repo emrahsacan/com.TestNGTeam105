6193e4f9746baaf70e4080772923f05af5fabff1
emrahsacan <you@example.com>
2023-01-07 19:28:16 +0300

    07/01/2023-1

---
+++ src/test/java/tests/day16_SmokeTests/C01_PozitifLoginTest.java
@@ -0,0 +1,5 @@
+package tests.day16_SmokeTests;
+
+public class C01_PozitifLoginTest {
+
+}
---
+++ src/test/java/tests/day16_SmokeTests/C03_SwitchhingWindows.java
@@ -0,0 +1,10 @@
+package tests.day16_SmokeTests;
+
+import org.testng.annotations.Test;
+
+public class C03_SwitchhingWindows {
+    @Test
+    public void test01(){
+
+    }
+}
